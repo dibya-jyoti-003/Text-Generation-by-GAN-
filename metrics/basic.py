b547dd3e19f4dd51963a0ec588141a9919cb555c
Dibyajyoti Karmakar <102862431+dibya-jyoti-003@users.noreply.github.com>
2025-04-25 12:35:41 +0530

    Update basic.py

--- metrics/basic.py
+++ metrics/basic.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Author       : William
-# @Project      : TextGAN-william
-# @FileName     : basic.py
-# @Time         : Created at 2019-05-14
-# @Blog         : http://zhiweil.ml/
-# @Description  : 
-# Copyrights (C) 2018. All Rights Reserved.
 
 from abc import abstractmethod
 
